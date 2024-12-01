42a037c1db64d859f95ab412d59a8a4e8348345b
radnichok <radnichokandreas@gmail.com>
2024-12-01 02:09:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// const follower = document.getElementById('follower');
+
+// let mouseX = 0, mouseY = 0; 
+// let posX = 0, posY = 0;     
+
+// document.addEventListener('mousemove', (event) => {
+//     mouseX = event.pageX; 
+//     mouseY = event.pageY; 
+// });
+
+// function animateFollower() {
+//     posX += (mouseX - posX) * 0.75; 
+//     posY += (mouseY - posY) * 0.75; 
+
+//     follower.style.transform = `translate(${posX}px, ${posY}px)`; 
+//     requestAnimationFrame(animateFollower); 
+// }
+
+// animateFollower();
+
+// function scrollToNext() {
+//     document.getElementById('next-section').scrollIntoView({
+//         behavior: 'smooth' 
+//     });
+// }
+// function scrollToSection(sectionId) {
+//     const section = document.getElementById(sectionId);
+//     section.scrollIntoView({ behavior: 'smooth' });
+// }
